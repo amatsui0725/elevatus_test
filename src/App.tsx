82126f90187dac5b74c8761cdece4f63c1dcdcd9
Akira Matsui <akira.matsui86@hotmail.com>
2022-11-29 08:54:51 -0800

    add internationlization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Box, Container, CssBaseline, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import JobsContainer from './container/Jobs';
 
 function App() {
+  const { t } = useTranslation();
   return (
     <>
       <CssBaseline />
       <Box sx={{ backgroundColor: '#000' }}>
         <Container fixed>
           <Box display="flex" justifyContent="space-between" alignItems="center" sx={{ height: 72 }}>
-            <Typography variant="h4" sx={{ textTransform: 'uppercase', color: '#fff' }}>Elevatus</Typography>
+            <Typography variant="h4" sx={{ textTransform: 'uppercase', color: '#fff' }}>{t("title")}</Typography>
             <Typography sx={{ color: '#fff' }}>Akira Matsui</Typography>
           </Box>
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './i18n';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
